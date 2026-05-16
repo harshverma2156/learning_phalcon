d5d4640f8781f2bfe5a14cf9b8e965ab07e85092
Harsh Verma <harshvermadev@gmail.com>
2026-05-16 12:21:44 +0530

    Implemented, How $this->view works.

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -6,7 +6,7 @@ class IndexController extends Controller
 {
     public function indexAction()
     {
-        $this->view->disable();
+        // $this->view->disable();
 
         echo "First Controller is working!";
     }
