a181efb2654fd679505c3b935aed1927b48da0ce
Harsh Verma <harshvermadev@gmail.com>
2026-05-19 11:06:46 +0530

    Updated Columns: Added New Columns(Number, City, State, Country, Pincode.

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -6,6 +6,11 @@ class Users extends Model {
     public $id;
     public $name;
     public $email;
+    public $number;
+    public $city;
+    public $state;
+    public $country;
+    public $pincode;
     public $created_at;
 
     public function initialize()
